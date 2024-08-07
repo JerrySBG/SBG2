8ac0f80bdfe77a786686b5c2c62d63e5c1ba8a73
JerrySBG <165622937+JerrySBG@users.noreply.github.com>
2024-08-07 18:38:40 -0500

    1.0

--- sshws/insshws.sh
+++ sshws/insshws.sh
@@ -4,43 +4,38 @@ url="https://raw.githubusercontent.com/JerrySBG/SBG2/main"
 cd
 
 #Install Script Websocket-SSH Python
-wget -O /usr/local/bin/proxy ${url}/sshws/https.py
-#wget -O /usr/local/bin/proxy2 ${url}/sshws/https2.py
-#wget -O /usr/local/bin/proxy3 ${url}/sshws/https3.py
+wget -O /usr/local/bin/edu-proxy ${url}/sshws/https.py
+#wget -O /usr/local/bin/edu-proxy2 ${url}/sshws/https2.py
+#wget -O /usr/local/bin/edu-proxy3 ${url}/sshws/https3.py
 wget -O /usr/local/bin/ws-dropbear ${url}/sshws/dropbear-ws.py
 wget -O /usr/local/bin/ws-stunnel ${url}/sshws/ws-stunnel.py
 
 #izin permision
-chmod +x /usr/local/bin/proxy
-#chmod +x /usr/local/bin/proxy2
-#chmod +x /usr/local/bin/proxy3
+chmod +x /usr/local/bin/edu-proxy
 chmod +x /usr/local/bin/ws-dropbear
 chmod +x /usr/local/bin/ws-stunnel
+#chmod +x /usr/local/bin/edu-proxyovpn
+#chmod +x /usr/local/bin/ws-openssh
 
 #System Direcly dropbear Websocket-SSH Python
-wget -O /etc/systemd/system/proxy.service ${url}/sshws/http.service && chmod +x /etc/systemd/system/proxy.service
-
-#System Direcly dropbear Websocket-SSH Python
-#wget -O /etc/systemd/system/proxy2.service ${url}/sshws/http2.service && chmod +x /etc/systemd/system/proxy2.service
-
-#System Direcly dropbear Websocket-SSH Python
-#wget -O /etc/systemd/system/proxy3.service ${url}/sshws/http3.service && chmod +x /etc/systemd/system/proxy3.service
+wget -O /etc/systemd/system/edu-proxy.service ${url}/sshws/http.service && chmod +x /etc/systemd/system/edu-proxy.service
+#wget -O /etc/systemd/system/edu-proxy2.service ${url}/sshws/http2.service && chmod +x /etc/systemd/system/edu-proxy2.service
+#wget -O /etc/systemd/system/edu-proxy3.service ${url}/sshws/http3.service && chmod +x /etc/systemd/system/edu-proxy3.service
 
 #System Dropbear Websocket-SSH Python
-wget -O /etc/systemd/system/ws-dropbear.service ${url}/sshws/service-wsdropbear.txt && chmod +x /etc/systemd/system/ws-dropbear.service
+wget -O /etc/systemd/system/ws-dropbear.service ${url}/sshws/service-wsdropbear && chmod +x /etc/systemd/system/ws-dropbear.service
 
 #System SSL/TLS Websocket-SSH Python
-wget -O /etc/systemd/system/ws-stunnel.service ${url}/sshws/ws-stunnel.service.txt && chmod +x /etc/systemd/system/ws-stunnel.service
+wget -O /etc/systemd/system/ws-stunnel.service ${url}/sshws/ws-stunnel.service && chmod +x /etc/systemd/system/ws-stunnel.service
 
 
 #restart service
 systemctl daemon-reload
 
 #Enable & Start & Restart directly dropbear
-#systemctl daemon-reload
-systemctl enable proxy.service
-systemctl start proxy.service
-systemctl restart proxy.service
+systemctl enable edu-proxy.service
+systemctl start edu-proxy.service
+systemctl restart edu-proxy.service
 #systemctl enable proxy2.service
 #systemctl start proxy2.service
 #systemctl restart proxy2.service
@@ -57,5 +52,4 @@ systemctl restart ws-dropbear.service
 systemctl enable ws-stunnel.service
 systemctl start ws-stunnel.service
 systemctl restart ws-stunnel.service
-
 clear
