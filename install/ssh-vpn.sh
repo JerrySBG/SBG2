0aea9b23b15728cc5a835b59f3e24c794d9783bb
JerrySBG <165622937+JerrySBG@users.noreply.github.com>
2024-08-07 18:37:46 -0500

    1.0

--- install/ssh-vpn.sh
+++ install/ssh-vpn.sh
@@ -230,19 +230,17 @@ socket = l:TCP_NODELAY=1
 socket = r:TCP_NODELAY=1
 
 [dropbear]
-accept = 445
-connect = 127.0.0.1:22
-
-[dropbear]
 accept = 447
 connect = 127.0.0.1:109
 
-#[dropbear]
-#accept = 2082
-#connect = 127.0.0.1:22
-#[dropbear]
-#accept = 2083
-#connect = 127.0.0.1:109
+[dropbear]
+accept = 2082
+connect = 127.0.0.1:22
+
+[dropbear]
+accept = 2083
+connect = 127.0.0.1:109
+
 #[ws-stunnel]
 #accept = 2083
 #connect = 700
@@ -331,12 +329,12 @@ wget https://raw.githubusercontent.com/JerrySBG/SBG2/main/install/bbr.sh && chmo
 #run_ip
 iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 80 -j ACCEPT
 iptables -I INPUT -m state --state NEW -m udp -p udp --dport 80 -j ACCEPT
-iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 8008 -j ACCEPT
-iptables -I INPUT -m state --state NEW -m udp -p udp --dport 8008 -j ACCEPT
-iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 8080 -j ACCEPT
-iptables -I INPUT -m state --state NEW -m udp -p udp --dport 8080 -j ACCEPT
-iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 8280 -j ACCEPT
-iptables -I INPUT -m state --state NEW -m udp -p udp --dport 8280 -j ACCEPT
+#iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 8008 -j ACCEPT
+#iptables -I INPUT -m state --state NEW -m udp -p udp --dport 8008 -j ACCEPT
+#iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 8080 -j ACCEPT
+#iptables -I INPUT -m state --state NEW -m udp -p udp --dport 8080 -j ACCEPT
+#iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 8280 -j ACCEPT
+#iptables -I INPUT -m state --state NEW -m udp -p udp --dport 8280 -j ACCEPT
 iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 443 -j ACCEPT
 iptables -I INPUT -m state --state NEW -m udp -p udp --dport 443 -j ACCEPT
 iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 2052 -j ACCEPT
