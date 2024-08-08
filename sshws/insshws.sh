c177ac19360ad4501ffed93a779a367c54ce4c36
JerrySBG <165622937+JerrySBG@users.noreply.github.com>
2024-08-08 16:45:04 -0500

    1.0

---
+++ sshws/insshws.sh
@@ -0,0 +1,120 @@
+#!/bin/bash
+# Proxy For Edukasi & Imclass
+# SL
+# ==========================================
+
+# Link Hosting Kalian
+JerrySBG="raw.githubusercontent.com/JerrySBG/SBG2/main/sshws"
+
+# Getting Proxy Template
+wget -q -O /usr/local/bin/ws-dropbear https://${JerrySBG}/websocket.py
+chmod +x /usr/local/bin/ws-dropbear
+
+# Installing Service
+cat > /etc/systemd/system/ws-nontls.service << END
+[Unit]
+Description=Python Proxy Mod By JERRY 
+Documentation=https://t.me/Jerry_SBG
+After=network.target nss-lookup.target
+
+[Service]
+Type=simple
+User=root
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=/usr/bin/python -O /usr/local/bin/ws-dropbear 80
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+END
+
+systemctl daemon-reload
+systemctl enable ws-dropbear.service
+systemctl start ws-dropbear.service
+systemctl restart ws-dropbear.service
+
+# Getting Proxy Template
+wget -q -O /usr/local/bin/ws-dropbear2 https://${JerrySBG}/websocket2.py
+chmod +x /usr/local/bin/ws-dropbear2
+
+# Installing Service
+cat > /etc/systemd/system/ws-nontls2.service << END
+[Unit]
+Description=Python Proxy Mod By Jerry
+Documentation=https://t.me/Jerry_SBG
+After=network.target nss-lookup.target
+
+[Service]
+Type=simple
+User=root
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=/usr/bin/python -O /usr/local/bin/ws-dropbear2 8080
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+END
+
+systemctl daemon-reload
+systemctl enable ws-dropbear2
+systemctl restart ws-dropbear2
+
+# Getting Proxy Template
+wget -q -O /usr/local/bin/ws-dropbear3 https://${JerrySBG}/websocket3.py
+chmod +x /usr/local/bin/ws-dropbear3
+
+# Installing Service
+cat > /etc/systemd/system/ws-nontls3.service << END
+[Unit]
+Description=Python Proxy Mod By Jerry
+Documentation=https://t.me/Jerry_SBG
+After=network.target nss-lookup.target
+
+[Service]
+Type=simple
+User=root
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=/usr/bin/python -O /usr/local/bin/ws-dropbear3 8280
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+END
+
+systemctl daemon-reload
+systemctl enable ws-dropbear3
+systemctl restart ws-dropbear3
+
+# Getting Proxy Template
+wget -O /usr/local/bin/ws-stunnel https://${JerrySBG}/ws-stunnel.txt
+chmod +x /usr/local/bin/ws-stunnel
+
+# Installing Service
+cat > /etc/systemd/system/ws-stunnel.service << END
+[Unit]
+Description=Python Proxy Mod By SFVPN
+Documentation=https://t.me/abecasdee13
+After=network.target nss-lookup.target
+
+[Service]
+Type=simple
+User=root
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=/usr/bin/python -O /usr/local/bin/ws-stunnel
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+END
+
+systemctl enable ws-stunnel.service
+systemctl start ws-stunnel.service
+systemctl restart ws-stunnel.service
