089f98cbcc9010735516eb3a2cb4535ee5cacbc6
JerrySBG <165622937+JerrySBG@users.noreply.github.com>
2024-08-07 22:52:42 -0500

    1.0

--- slowdns/installsl.sh
+++ slowdns/installsl.sh
@@ -12,6 +12,7 @@ echo -e "${BIBlue}│  [ 2 ]  \033[1;37mDominio Aletorio Free             ${NC}"
 echo -e "${BIBlue}│  [ 3 ]  \033[1;37mDominio Aletorio By JERRY             ${NC}"                                        
 echo -e "${BIBlue}╰═══════════════════════════════════════════╯${NC}"
 read -p "   Por Favor Selecciona del 1 al 3 : " slow
+echo ""
 if [[ $slow == "1" ]]; then
 echo -e "   \e[1;32mPor favor Ingrese su Subdominio SlowDNS$NC"
 read -p "   Subdominio: " slow
@@ -19,15 +20,12 @@ mkdir -p /etc/xray
 touch /etc/xray/nsdomain
 echo "$dns2" > /etc/xray/nsdomain
 echo "$dns2" >/etc/xray/dns
-echo ""
+fi
 clear
 elif [[ $slow == "2" ]]; then
 #install slowdns
 wget ${REPO}slowdns/installsl2.sh && chmod +x installsl2.sh && ./installsl2.sh
 rm -f /root/installsl2.sh
-clear
-else
-print_install "Se utiliza Subdominio/Dominio Aleatorio"
 clear
 elif [[ $slow == "3" ]]; then
 #install slowdns
