f7cb0a67401d190c3eac05174ed44b59ff7b85d6
JerrySBG <165622937+JerrySBG@users.noreply.github.com>
2024-08-07 22:23:44 -0500

    1.0

--- slowdns/installsl.sh
+++ slowdns/installsl.sh
@@ -8,12 +8,13 @@ echo -e "${BIBlue}│\033[1;37m Seleccione Opcion para Configurar Dominio ${BIBl
 echo -e "${BIBlue}╰═══════════════════════════════════════════╯${NC}"
 echo -e "${BIBlue}╭═══════════════════════════════════════════╮${NC}"
 echo -e "${BIBlue}│  [ 1 ]  \033[1;37mPropio Dominio SlowDNS                ${NC}"  
-echo -e "${BIBlue}│  [ 2 ]  \033[1;37mDominio Aletorio By JERRY              ${NC}"                                        
+echo -e "${BIBlue}│  [ 2 ]  \033[1;37mDominio Aletorio Free             ${NC}" 
+echo -e "${BIBlue}│  [ 3 ]  \033[1;37mDominio Aletorio By JERRY             ${NC}"                                        
 echo -e "${BIBlue}╰═══════════════════════════════════════════╯${NC}"
-read -p "   Seleccione los Números 1 o 2 Cualquier Botón (Aleatorio) : " slow
+read -p "   Seleccione los Números 1 o 3 : " slow
 echo ""
 if [[ $slow == "1" ]]; then
-echo -e "   \e[1;32mPor favor ingrese su Subdominio SlowDNS$NC"
+echo -e "   \e[1;32mPor favor Ingrese su Subdominio SlowDNS$NC"
 read -p "   Subdominio: " slow
 mkdir -p /etc/xray
 touch /etc/xray/nsdomain
@@ -22,6 +23,14 @@ echo "$dns2" >/etc/xray/dns
 echo ""
 clear
 elif [[ $slow == "2" ]]; then
+#install slowdns
+wget ${REPO}slowdns/installsl2.sh && chmod +x installsl2.sh && ./installsl2.sh
+rm -f /root/installsl2.sh
+clear
+else
+print_install "Se utiliza Subdominio/Dominio Aleatorio"
+clear
+elif [[ $slow == "3" ]]; then
 #install slowdns
 wget ${REPO}slowdns/slowdns.sh && chmod +x slowdns.sh && ./slowdns.sh
 rm -f /root/slowdns.sh
