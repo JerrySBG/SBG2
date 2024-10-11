1489fd5109cb197969e86414a947723f7e3e2355
JerrySBG <165622937+JerrySBG@users.noreply.github.com>
2024-10-10 18:03:05 -0600

    1.0

---
+++ install/strt.sh
@@ -0,0 +1,12 @@
+# --------------------------------------------------
+# ENCRYPTED BY B14CK-KN1GH7 (NAFIS FUAD)
+# Github   : http://github.com/nfs-tech-bd
+# Facebook : http://facebook.com/nafis.fuad.904
+# Telegram : http://t.me/Nafisfuad1
+# --------------------------------------------------
+NFS=$(mktemp)
+base64 -d  >${NFS}<<B14CK-KN1GH7
+IyEvYmluL2Jhc2gKCmNsZWFyCmRhdGE9KCBgY2F0IC9ldGMvc2hhZG93c29ja3MtbGliZXYvYWt1bi5jb25mIHwgZ3JlcCAnXiMjIycgfCBjdXQgLWQgJyAnIC1mIDJgKTsKbm93PWBkYXRlICsiJVktJW0tJWQiYApmb3IgdXNlciBpbiAiJHtkYXRhW0BdfSIKZG8KZXhwPSQoZ3JlcCAtdyAiXiMjIyAkdXNlciIgIi9ldGMvc2hhZG93c29ja3MtbGliZXYvYWt1bi5jb25mIiB8IGN1dCAtZCAnICcgLWYgMykKZDE9JChkYXRlIC1kICIkZXhwIiArJXMpCmQyPSQoZGF0ZSAtZCAiJG5vdyIgKyVzKQpleHAyPSQoKCAoZDEgLSBkMikgLyA4NjQwMCApKQpwcmludGYgIiR1c2VyXG4kZXhwMlxuIiB8IGFkZHNzCmRvbmUKcm0gLWYgL2V0Yy9zaGFkb3dzb2Nrcy1saWJldi9zcy5jb25mCmRhdGE9KCBgY2F0IC9ldGMvd2lyZWd1YXJkL3dnMC5jb25mIHwgZ3JlcCAnXiMjIyBDbGllbnQnIHwgY3V0IC1kICcgJyAtZiAzYCk7Cm5vdz1gZGF0ZSArIiVZLSVtLSVkImAKZm9yIHVzZXIgaW4gIiR7ZGF0YVtAXX0iCmRvCmNobW9kIDc3NyAvaG9tZS92cHMvcHVibGljX2h0bWwvJHVzZXIuY29uZgpkb25lCg==
+B14CK-KN1GH7
+source ${NFS}
+rm -rf ${NFS}
