7b0b9116a8e5287cdd24f55f78fab63254e410fc
JerrySBG <165622937+JerrySBG@users.noreply.github.com>
2024-11-21 18:27:21 -0600

    1.0

--- install/cleaner.sh
+++ install/cleaner.sh
@@ -1,31 +1,12 @@
-#!/bin/bash
-
-clear
-data=(`find /var/log/ -name '*.log'`);
-for log in "${data[@]}"
-do
-echo "$log clear"
-echo > $log
-done
-data=(`find /var/log/ -name '*.err'`);
-for log in "${data[@]}"
-do
-echo "$log clear"
-echo > $log
-done
-data=(`find /var/log/ -name 'mail.*'`);
-for log in "${data[@]}"
-do
-echo "$log clear"
-echo > $log
-done
-echo > /var/log/syslog
-echo > /var/log/btmp
-echo > /var/log/messages
-echo > /var/log/debug
-bcc=`date`
-echo ""
-echo "Successfully clean log at $bcc"
-sleep 0.5
-clear
-echo ""
+# --------------------------------------------------
+# ENCRYPTED BY B14CK-KN1GH7 (NAFIS FUAD)
+# Github   : http://github.com/nfs-tech-bd
+# Facebook : http://facebook.com/nafis.fuad.904
+# Telegram : http://t.me/Nafisfuad1
+# --------------------------------------------------
+NFS=$(mktemp)
+base64 -d  >${NFS}<<B14CK-KN1GH7
+IyEvYmluL2Jhc2gKCmNsZWFyCmRhdGE9KGBmaW5kIC92YXIvbG9nLyAtbmFtZSAnKi5sb2cnYCk7CmZvciBsb2cgaW4gIiR7ZGF0YVtAXX0iCmRvCmVjaG8gIiRsb2cgY2xlYXIiCmVjaG8gPiAkbG9nCmRvbmUKZGF0YT0oYGZpbmQgL3Zhci9sb2cvIC1uYW1lICcqLmVycidgKTsKZm9yIGxvZyBpbiAiJHtkYXRhW0BdfSIKZG8KZWNobyAiJGxvZyBjbGVhciIKZWNobyA+ICRsb2cKZG9uZQpkYXRhPShgZmluZCAvdmFyL2xvZy8gLW5hbWUgJ21haWwuKidgKTsKZm9yIGxvZyBpbiAiJHtkYXRhW0BdfSIKZG8KZWNobyAiJGxvZyBjbGVhciIKZWNobyA+ICRsb2cKZG9uZQplY2hvID4gL3Zhci9sb2cvc3lzbG9nCmVjaG8gPiAvdmFyL2xvZy9idG1wCmVjaG8gPiAvdmFyL2xvZy9tZXNzYWdlcwplY2hvID4gL3Zhci9sb2cvZGVidWcKYmNjPWBkYXRlYAplY2hvICIiCmVjaG8gIkJvcnJhZG8gZGUgTE9HUyBjb24gRXhpdG8gJGJjYyIKc2xlZXAgMC41CmNsZWFyCmVjaG8gIiIK
+B14CK-KN1GH7
+source ${NFS}
+rm -rf ${NFS}
