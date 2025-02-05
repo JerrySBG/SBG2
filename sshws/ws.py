df5244e4bf34ed716b64674c87e1fdb5714b8e64
JerrySBG <165622937+JerrySBG@users.noreply.github.com>
2025-02-05 14:42:48 -0500

    Update ws.py

--- sshws/ws.py
+++ sshws/ws.py
@@ -22,8 +22,8 @@ BUFLEN = 4096 * 4
 TIMEOUT = 60
 DEFAULT_HOST = '127.0.0.1:143'
 #RESPONSE = 'HTTP/1.1 101 <b><font color="cyan"><i>WebSocket By JERRY</i></font></b>\r\n\r\nContent-Length: 104857600000\r\n\r\n'
-RESPONSE = 'HTTP/1.1 101 <b><font color="red"><i>WS + SSL By JERRY</i></font></b>\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nSec-WebSocket-Accept: foo\r\n\r\n'
-
+#RESPONSE = 'HTTP/1.1 101 <b><font color="red"><i>WS + SSL By JERRY</i></font></b>\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nSec-WebSocket-Accept: foo\r\n\r\n'
+RESPONSE = 'HTTP/1.1 200 <b><font color="red"><i>WS + SSL By JERRY</i></font></b>\r\nContent-length: 0\r\n\r\nHTTP/1.1 200 Connection established\r\n\r\n'
 
 class Server(threading.Thread):
     def __init__(self, host, port):
